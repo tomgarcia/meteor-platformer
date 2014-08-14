5ee83b01a79adaf87bf445ffbe1bdd24f24637ca
thomas <thomasjgarcia1@gmail.com>
2014-08-14 13:46:42 -0700

    Game can now be paused, and restarted. Game automatically ends after 1 minute

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ meteor-platformer
 A basic platformer for the meteor cluster.
 To start either the server or the client, run game.py.
 All settings are in game.conf.
+Controls are WASD for player 1, and arrow keys for player 2. Pause the game with 'p', and restart the game with 'r'.
--- game.py
+++ game.py
@@ -14,6 +14,7 @@ from random import *
 parser = SafeConfigParser()
 parser.read('game.conf')
 port = parser.getint('connection', 'port')
+pausedFont = pygame.font.SysFont('helvetica', 100)
 
 server = None #Acts as flag for servermode, and as the actual server object
 if raw_input('Server (y/n)? ') == 'y':
@@ -21,7 +22,7 @@ if raw_input('Server (y/n)? ') == 'y':
 	WIN_SIZE = WIN_WIDTH, WIN_HEIGHT = parser.getint('window', 'width'), parser.getint('window', 'height')
 	ROWS = parser.getint('window', 'rows')
 	COLS = parser.getint('window', 'cols')
-	seed = randint(0, 100000)
+	seed = randint(0, 99999999999)
 	constants = {'width': WIN_WIDTH, 'height': WIN_HEIGHT, 'rows': ROWS, 'cols': COLS, 'seed': seed}
 	server = Server(address, json.dumps(constants))
 	server.start()
@@ -51,39 +52,57 @@ clock = pygame.time.Clock()
 world = World(width, height, seed)
 player = world.player1
 player2 = world.player2
+paused = False
+gameOver = False
+time = 0
 
 #Method for pygame events, for both server and client
 def handleEvents(events):
+	global time, paused, gameOver, world, player, player2
 	for event in events:
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
+		elif event.type == USEREVENT and event.code == "GAME_OVER":
+			gameOver = True
 		elif event.type == KEYDOWN:
-			if event.key == K_LEFT:
-				player.setDirection(LEFT)
-			elif event.key == K_RIGHT:
-				player.setDirection(RIGHT)
-			elif event.key == K_UP:
-				player.jump()
-			elif event.key == K_a:
+			if event.key == K_p:
+				paused = not paused
+			elif event.key == K_r:
+				paused = False
+				gameOver = False
+				time = 0
+				world = World(width, height, seed)
+				player = world.player1
+				player2 = world.player2
+			elif event.key == K_LEFT:
 				player2.setDirection(LEFT)
-			elif event.key == K_d:
+			elif event.key == K_RIGHT:
 				player2.setDirection(RIGHT)
-			elif event.key == K_w:
+			elif event.key == K_UP and not paused:
 				player2.jump()
+			elif event.key == K_a:
+				player.setDirection(LEFT)
+			elif event.key == K_d:
+				player.setDirection(RIGHT)
+			elif event.key == K_w and not paused:
+				player.jump()
 		elif event.type == KEYUP:
 			if event.key == K_LEFT:
-				player.setDirection(RIGHT)
-			elif event.key == K_RIGHT:
-				player.setDirection(LEFT)
-			elif event.key == K_a:
 				player2.setDirection(RIGHT)
-			elif event.key == K_d:
+			elif event.key == K_RIGHT:
 				player2.setDirection(LEFT)
+			elif event.key == K_a:
+				player.setDirection(RIGHT)
+			elif event.key == K_d:
+				player.setDirection(LEFT)
 
 while True:
 	if server:
 		events = pygame.event.get()
+		if time >= 60000 and not gameOver:
+			gameOver = True
+			events.append(pygame.event.Event(USEREVENT, {'code': 'GAME_OVER'}))
 		eventsJson = json.dumps([(event.type, event.__dict__) for event in events])
 		server.send(eventsJson)
 	else:
@@ -98,8 +117,30 @@ while True:
 		s = s[0:length]#cut off the extra bytes
 		events = [pygame.event.Event(event[0], event[1]) for event in json.loads(s)]
 	handleEvents(events)
-	world.update()
-	area = pygame.Rect(coords[0] * screen.get_width(), coords[1] * screen.get_height(), screen.get_width(), screen.get_height()) #The area of the world to get
-	screen.blit(world.getSurface(area), (0, 0))
+	if gameOver:
+		if player.score > player2.score:
+			text = 'Player 1 won'
+		elif player2.score > player.score:
+			text = 'Player 2 won'
+		else:
+			text = 'Tie'
+		msg = pausedFont.render('Game Over', 1, (0, 0, 0))
+		winnermsg = pausedFont.render(text, 2, (0, 0, 0))
+		words = pygame.Surface((max(msg.get_width(), winnermsg.get_width()), msg.get_height() + winnermsg.get_height()))
+		words.fill((255, 255, 255))
+		words.blit(msg, [0, 0])
+		words.blit(winnermsg, [0, msg.get_height()])
+		loc = (screen.get_width() / 2 - words.get_width() / 2, screen.get_height() / 2 - words.get_height() / 2)
+		screen.blit(words, loc)
+	elif not paused:
+		world.update()
+		area = pygame.Rect(coords[0] * screen.get_width(), coords[1] * screen.get_height(), screen.get_width(), screen.get_height()) #The area of the world to get
+		screen.blit(world.getSurface(area), (0, 0))
+	elif paused:
+		words = pausedFont.render('Paused', 1, (0, 0, 0))
+		loc = (screen.get_width() / 2 - words.get_width() / 2, screen.get_height() / 2 - words.get_height() / 2)
+		screen.blit(words, loc)
 	pygame.display.update()
-	clock.tick(60)
+	ctr = clock.tick(60)
+	if not paused:
+		time += ctr
